57bb2c48c372a91367000532dc06145767d54c33
Pietro Meloni <pietromeloni@hotmail.it>
2018-04-30 17:12:13 +0200

    WS-2018.1 <PietroMeloni@DESKTOP-6TKA2JR Overwrite remote https://github.com/davidespano/D3P2 to local

--- spinner.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var spinnerX = $("#spinner_pos_x").spinner( );
-    var spinnerY = $("#spinner_pos_y").spinner(  );
-    var spinnerZ = $("#spinner_pos_z").spinner(  );
-
-
-
-
-});
