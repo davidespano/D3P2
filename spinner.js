cd20f6d5c3c8cfe87e154fe7347a13fd872eeffa
Pietro Meloni <pietromeloni@hotmail.it>
2018-05-01 19:53:39 +0200

    Integration of help and CSS Major Changes

---
+++ spinner.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     var spinnerX = $("#spinner_pos_x").spinner( );
+//     var spinnerY = $("#spinner_pos_y").spinner(  );
+//     var spinnerZ = $("#spinner_pos_z").spinner(  );
+//
+//
+//
+//
+// });
